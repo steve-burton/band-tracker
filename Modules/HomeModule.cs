543f6562aac8f913efa104635a394c7745993e6d
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2016-12-16 12:22:39 -0800

    write HomeModule routes to add bands and venues and view all bands and venues

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,9 +10,32 @@ namespace BandTracker
 	{
 		public HomeModule()
 		{
-			Get["/"] = _ =>
-			{
+			Get["/"] = _ => {
 				return View["index.cshtml"];
+			};
+			Get["/add-new-band"] = _ => {
+				return View["/add-new-band.cshtml"];
+			};
+			Get["/add-new-venue"] = _ => {
+				return View["/add-new-venue.cshtml"];
+			};
+			Get["/all-bands"] = _ => {
+				List<Band> allBands = Band.GetAll();
+				return View["/all-bands.cshtml", allBands];
+			};
+			Get["/all-venues"] = _ => {
+				List<Venue> allVenues = Venue.GetAll();
+				return View["/all-venues.cshtml", allVenues];
+			};
+			Post["/added-band"] = _ => {
+				Band newBand = new Band(Request.Form["band-name"]);
+				newBand.Save();
+				return View["/success.cshtml"];
+			};
+			Post["/added-venue"] = _ => {
+				Venue newVenue = new Venue(Request.Form["venue-name"]);
+				newVenue.Save();
+				return View["/success.cshtml"];
 			};
 		}
 	}
